ff097b1113f6c7dcc5dcda9fcaafcc12e7c2f399
Scott O'Brien <scott@scottyob.com>
2023-12-22 13:49:41 -0800

    Removing stuff

--- prisma/migrations/20231210194155_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Pattern" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "pattern" TEXT NOT NULL,
-    "currentSequence" INTEGER NOT NULL DEFAULT 1
-);
