c0938efb4731a0578797f89392da41ca2871a049
Scott O'Brien <scott@scottyob.com>
2023-12-22 18:13:16 -0800

    Working page, requires server

---
+++ app/home.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useLocalStorage } from "@uidotdev/usehooks";
+import AComponent from "./AComponent";
+import { AppStorage, DefaultAppState } from "./types";
+import { kebabCase } from "lodash";
+
+export default function Home() {
+  let [localStore, setLocalStore] = useLocalStorage<AppStorage>(
+    "stitch",
+    DefaultAppState
+  );
+
+  return (
+    <div>
+      <h1 className="text-6xl p-6 text-center font-bold shadow-md text-orange-800">SPACE-STITCH</h1>
+      <h2 className="text-xl p-4">Projects</h2>
+      <div className="m-4 pl-4">
+        <ul className="list-disc">
+          {Object.keys(localStore.patterns).map((patternName) => {
+            const deleteMethod = () => {
+              delete localStore.patterns[patternName];
+              setLocalStore(localStore);
+            };
+
+            return (
+              <li key={patternName}>
+                <a href={`/play/${kebabCase(patternName)}`} className="underline text-orange-500 decoration-orange-200">{patternName}</a>
+                <span className="text-xs">
+                  {" - "}
+                  <a href={`/edit/${kebabCase(patternName)}`}>(edit)</a>
+                  <AComponent text={"(delete)"} onClick={() => {}} />
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <a href="/edit/new" className="bg-blue-300 rounded-lg p-2 m-8">
+        New Project
+      </a>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,11 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useLocalStorage } from "@uidotdev/usehooks";
-import AComponent from "./AComponent";
-import { AppStorage, DefaultAppState } from "./types";
-import { kebabCase } from "lodash";
+export default async function Page() {
 
-export default function Index() {
-  let [localStore, setLocalStore] = useLocalStorage<AppStorage>(
-    "stitch",
-    DefaultAppState
-  );
+  const res = await dynamic(() => import('./home'), {
+    ssr: false
+  });
+  // @ts-expect-error no idea why this is needed to be SSR disabled
+  return res();
 
-  return (
-    <div>
-      <h1 className="text-6xl p-6 text-center font-bold shadow-md text-orange-800">SPACE-STITCH</h1>
-      <h2 className="text-xl p-4">Projects</h2>
-      <div className="m-4 pl-4">
-        <ul className="list-disc">
-          {Object.keys(localStore.patterns).map((patternName) => {
-            const deleteMethod = () => {
-              delete localStore.patterns[patternName];
-              setLocalStore(localStore);
-            };
-
-            return (
-              <li key={patternName}>
-                <a href={`/play/${kebabCase(patternName)}`} className="underline text-orange-500 decoration-orange-200">{patternName}</a>
-                <span className="text-xs">
-                  {" - "}
-                  <a href={`/edit/${kebabCase(patternName)}`}>(edit)</a>
-                  <AComponent text={"(delete)"} onClick={deleteMethod} />
-                </span>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-
-      <a href="/edit/new" className="bg-blue-300 rounded-lg p-2 m-8">
-        New Project
-      </a>
-    </div>
-  );
 }
